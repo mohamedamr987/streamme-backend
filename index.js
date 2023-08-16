5b5ad6548d2f2d1bbca8c9cc76a2adfadc7a63fd
Mohamed Amr <81720517+mohamedamr987@users.noreply.github.com>
2023-08-17 02:32:06 +0300

    ss

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const morgan = require("morgan");
+const rateLimiter = require("express-rate-limit");
+const helmet = require("helmet");
+const expressMongoSanitize = require("express-mongo-sanitize");
+const xss = require("xss-clean");
+const hpp = require("hpp");
+const AppError = require("./utils/appError");
+const userRoute = require("./routes/userRoute");
+const errorController = require("./controllers/errorController");
+
+const app = express();
+
+app.use(helmet());
+app.use(expressMongoSanitize());
+app.use(xss());
+app.use(hpp());
+app.use(express.static("public"));
+app.use("/images", express.static("images"));
+
+if (process.env.NODE_ENV !== "production") {
+  app.use(morgan("dev"));
+}
+app.use(express.json());
+
+const baseV1ApiRoute = "/api/v1/";
+const limiter = rateLimiter({
+  max: 1000,
+  windowMs: 60 * 60 * 1000,
+  message: "Rate limit exceeded",
+});
+
+app.use(`${baseV1ApiRoute}`, limiter);
+
+app.use(`${baseV1ApiRoute}users`, userRoute);
+
+app.all("*", (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server`, 404));
+});
+
+app.use(errorController);
+
+module.exports = app;
--- server.js
+++ server.js
@@ -1,17 +1,17 @@
-const dotenv = require('dotenv');
-const mongoose = require('mongoose');
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
 
-dotenv.config({ path: './config.env' });
-const app = require('./app');
+dotenv.config({ path: "./config.env" });
+const app = require(".");
 
 mongoose
   .connect(process.env.DATABASE)
-  .then(() => console.log('connected to database'));
+  .then(() => console.log("connected to database"));
 
 const port = process.env.PORT;
 
 const server = app.listen(port, () => {
-  console.log('Example app listening on port 3000!');
+  console.log("Example app listening on port 3000!");
 });
 
 // process.on('unhandledRejection', (err) => {
