5771d3a290c9096f91ef0e43940855257b800496
Mohamed Amr <81720517+mohamedamr987@users.noreply.github.com>
2023-08-18 21:46:22 +0300

    fixed

--- controllers/agoraController.js
+++ controllers/agoraController.js
@@ -5,7 +5,7 @@ const appID = process.env.AGORA_APP_ID;
 const appCertificate = process.env.AGORA_APP_CERTIFICATE;
 
 // Build token with uid
-function generateToken(channelName, uid = 0, role) {
+exports.generateToken = function (channelName, uid = 0, role) {
   const expirationTimeInSeconds = 3600;
   const currentTimestamp = Math.floor(Date.now() / 1000);
   const privilegeExpiredTs = currentTimestamp + expirationTimeInSeconds;
@@ -18,8 +18,6 @@ function generateToken(channelName, uid = 0, role) {
     role,
     privilegeExpiredTs
   );
-  // console.log(token);
+  console.log(token);
   return token;
-}
-
-exports.generateToken = generateToken;
+};
--- controllers/streamController.js
+++ controllers/streamController.js
@@ -1,4 +1,4 @@
-const generateToken = require("./agoraController");
+const agoraController = require("./agoraController");
 const catchSync = require("../utils/catchSync");
 const AppError = require("../utils/appError");
 const Stream = require("../models/streamModel");
@@ -10,12 +10,14 @@ exports.getAllStreams = handlerFactory.getAll(Stream);
 exports.createStream = catchSync(async (req, res, next) => {
   const user = req.user._id;
   const stream = await Stream.create({ channelName: user, user });
-  const token = generateToken(channelName, RtcRole.PUBLISHER);
+
+  const token = agoraController.generateToken(user, RtcRole.PUBLISHER);
+  console.log(stream);
   res.status(201).json({
     isError: false,
     message: "Stream created successfully",
     data: {
-      ...stream,
+      stream,
       token,
     },
   });
@@ -34,7 +36,7 @@ exports.joinStream = catchSync(async (req, res, next) => {
     isError: false,
     message: "Stream joined successfully",
     data: {
-      ...stream,
+      stream,
       token,
     },
   });
