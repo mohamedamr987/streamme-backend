c8074e74e55a6eca3f3299f99e2cd5901e503c2a
Mohamed Amr <81720517+mohamedamr987@users.noreply.github.com>
2023-08-19 02:30:29 +0300

    s

--- controllers/agoraController.js
+++ controllers/agoraController.js
@@ -5,7 +5,8 @@ const appID = process.env.AGORA_APP_ID;
 const appCertificate = process.env.AGORA_APP_CERTIFICATE;
 
 // Build token with uid
-exports.generateToken = function (channelName, uid = 0, role) {
+exports.generateToken = function (channelName, role) {
+  const uid = 0;
   const expirationTimeInSeconds = 3600;
   const currentTimestamp = Math.floor(Date.now() / 1000);
   const privilegeExpiredTs = currentTimestamp + expirationTimeInSeconds;
